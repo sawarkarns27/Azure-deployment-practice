73d4822461357f85d3ee384f09fb8d262eab48be
neha wasankar <sawarkar.ns27@gmail.com>
2026-09-13 16:52:02 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddTutorial from "./components/add-tutorial.component";
+import CreateUser from "./components/create-user.component";
 import Tutorial from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
+import UsersList from "./components/users-list.component";
 
 class App extends Component {
   render() {
@@ -17,13 +17,13 @@ class App extends Component {
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
+              <Link to={"/users-list"} className="nav-link">
+                Users
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
+              <Link to={"/create-user"} className="nav-link">
+                Create User
               </Link>
             </li>
           </div>
@@ -31,10 +31,10 @@ class App extends Component {
 
         <div className="container mt-3">
           <Routes>
-            <Route path="/" element={<TutorialsList/>} />
-            <Route path="/tutorials" element={<TutorialsList/>} />
-            <Route path="/add" element={<AddTutorial/>} />
-            <Route path="/tutorials/:id" element={<Tutorial/>} />
+            <Route path="/" element={<UsersList/>} />
+            <Route path="/users-list" element={<UsersList/>} />
+            <Route path="/create-user" element={<CreateUser/>} />
+            {/* <Route path="/tutorials/:id" element={<Tutorial/>} /> */}
           </Routes>
         </div>
       </div>
